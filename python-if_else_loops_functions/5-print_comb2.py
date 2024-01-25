3792994b814eec6ad1857ad718812c173192bc8b
leomar <leomar.rodriguez1@upr.edu>
2024-01-25 16:34:27 -0400

    fixed formating in task 4

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(100):
     if (i < 10):
-        print("{:02}".format(i))
+        print("{:02}".format(i), end = ', ')
     else:
-        print("{}".format(i), end = '')
+        print("{}".format(i) if i == 99 else "{},".format(i), end = '\n' if i == 99 else '')
