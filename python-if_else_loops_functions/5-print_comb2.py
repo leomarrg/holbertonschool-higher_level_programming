e3d3486e1c07789f7ac684b08eff23a591ab7902
leomar <leomar.rodriguez1@upr.edu>
2024-01-25 13:26:28 -0400

    fixed formatting error in file 5

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(100):
     if (i < 10):
-        print(f"0{i}", end = ', ')
+        print("{:02}".format(i))
     else:
-        print(f"{i}, " if i < 99 else f"{i}", end = '\n' if i == 99 else '')
+        print("{}".format(i))
