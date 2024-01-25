1287e03859c454f842764a7466648ffc2b3323ef
leomar <leomar.rodriguez1@upr.edu>
2024-01-25 18:26:20 -0400

    fixed format in task 3

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -3,4 +3,4 @@ charCode = 61
 for charCode in range(ord('a'), ord('z')):
     if (charCode == ord('q') or charCode == ord('e')):
         continue
-    print(chr(charCode), end = '')
+    print("{}".format(chr(charCode)), end = '')
