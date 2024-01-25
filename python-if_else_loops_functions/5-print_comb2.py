4a3664e8fd61d764c5a4b13e78cbf5e84014f3e0
leomar <leomar.rodriguez1@upr.edu>
2024-01-25 16:38:11 -0400

    added space after comma

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -3,4 +3,4 @@ for i in range(100):
     if (i < 10):
         print("{:02}".format(i), end = ', ')
     else:
-        print("{}".format(i) if i == 99 else "{},".format(i), end = '\n' if i == 99 else '')
+        print("{}".format(i) if i == 99 else "{}, ".format(i), end = '\n' if i == 99 else '')
