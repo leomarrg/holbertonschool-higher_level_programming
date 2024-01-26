8209b16be2271600dee637c1b6c2742f47a92fbb
leomar <leomar.rodriguez1@upr.edu>
2024-01-26 14:56:40 -0400

    fixing print format in task 6

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,5 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for num in row:
-            print("{:d}".format(num), end=" ")
+            print("{:f}".format(num), end=" ")
         print()
