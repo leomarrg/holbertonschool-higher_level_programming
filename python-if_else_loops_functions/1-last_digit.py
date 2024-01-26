364757e3834058b6fbe034c04986e846cf21af92
leomar <leomar.rodriguez1@upr.edu>
2024-01-26 15:15:34 -0400

    fixed error in getting number less than 6

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -8,5 +8,5 @@ if (number % 10 > 5):
     print(f"{strLD} {number} {strIs} {lastDig} and is greater than 5")
 elif (number % 10 == 0):
     print(f"{strLD} {number} {strIs} {lastDig} and is 0")
-elif (number % 10 < 6):
+elif (number % 10 < 0):
     print(f"{strLD} {number} {strIs} {lastDig} and is less than 6 and not 0")
