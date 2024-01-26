b8794a082d32fca8b67c759732af18b0c1d000b8
leomar <leomar.rodriguez1@upr.edu>
2024-01-26 14:45:28 -0400

    pushing data structure tasks to test checkers

---
+++ python-data_structures/4-new_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    newList = my_list
+    if idx < 0:
+        return my_list
+    elif idx >= len(my_list):
+        return my_list
+    else:
+        newList[idx] = element
+        return newList
