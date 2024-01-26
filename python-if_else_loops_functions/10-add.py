87fe291ca199b0f5f2094b40cd8932fc27fb46b5
leomar <leomar.rodriguez1@upr.edu>
2024-01-26 12:56:53 -0400

    fixed first line on multiple files

--- python-if_else_loops_functions/10-add.py
+++ python-if_else_loops_functions/10-add.py
@@ -1,3 +1,3 @@
-#!/usr/bin/env python3
+#!/usr/bin/envpython3
 def add(a, b):
     return a + b
--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 def uppercase(str):
     for char in str:
         if ord(char) >= 97 and ord(char) <= 122:
