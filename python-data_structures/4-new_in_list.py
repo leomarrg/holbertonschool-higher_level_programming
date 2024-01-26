2bd009b7c15a97641925bffc4bc332c3bd495ffb
leomar <leomar.rodriguez1@upr.edu>
2024-01-26 15:02:41 -0400

    fixed errors at the end of file, testing new code to test checkers

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    newList = my_list
-    if idx < 0:
-        return my_list
-    elif idx >= len(my_list):
-        return my_list
-    else:
-        newList[idx] = element
-        return newList
+    if idx < 0 or idx > (len(my_list) - 1):
+        return (my_list)
+    copy = [i for i in my_list]
+    copy[idx] = element
+    return (copy)
     
