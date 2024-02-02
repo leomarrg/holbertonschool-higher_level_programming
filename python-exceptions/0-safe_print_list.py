299d3d3f0f605a0810d163a9ec5dcfabdd2e7806
leomar <leomar.rodriguez1@upr.edu>
2024-02-02 16:36:47 -0400

    fixed pycodestyle errors

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
         for i in range(x):
             print(my_list[i], end='')
             counter += 1
-    except:
+    except IndexError:
         pass
     print()
     return counter
