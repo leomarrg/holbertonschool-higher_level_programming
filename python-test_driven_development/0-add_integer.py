b692ee3694d38a63f05a7bcf3c5f9695d99119d4
leomar <leomar.rodriguez1@upr.edu>
2024-02-02 15:47:50 -0400

    changed second if to b

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -18,7 +18,7 @@ def add_integer(a, b=98):
         raise TypeError("a must be an integer")
     
     if(type(b) not in (int, float)):
-        raise TypeError("a must be an integer")
+        raise TypeError("b must be an integer")
 
     a = int(a)
     b = int(b)
