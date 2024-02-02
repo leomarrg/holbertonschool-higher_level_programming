5daaa923ee5d7d9e91db4782e243c3012c02a18a
leomar <leomar.rodriguez1@upr.edu>
2024-02-02 16:02:44 -0400

    testing common elem tasks, pushing task 12

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
     multiply_numbers = map(lambda x: x * number, my_list)
-    new_list = list(multiply_numbers)
-    return new_list
+    return multiply_numbers
--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -1,5 +1,2 @@
-#!/usr/bin/python3
 def common_elements(set_1, set_2):
-    if (set_1 & set_2):
-        my_set = set_1.intersection(set_2)
-        return my_set
+    return (set_1 & set_2)
