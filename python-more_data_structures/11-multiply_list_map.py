cbe9272aef780815279ab7e2364a3a6141e55188
leomar <leomar.rodriguez1@upr.edu>
2024-02-02 16:30:55 -0400

    added code to wrong file

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
-    multiply_numbers = map(lambda x: x * number, my_list)
-    return multiply_numbers
+    return list(map(lambda x: x * number, my_list))
