d31afb2c446c278b39506017fc03313738d41689
leomar <leomar.rodriguez1@upr.edu>
2024-02-16 14:04:13 -0400

    pushing task 9 and 10 to test checkers

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -107,16 +107,21 @@ class Rectangle(Base):
         return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id,
                                                        self.x, self.y,
                                                        self.width, self.height)
-
-    def update(self, *args):
+    """Task 7"""
+    """Task 8 is to add kwargs"""
+    def update(self, *args, **kwargs):
         """Public method that updates attributes"""
-        if len(args) >= 1:
-            self.id = args[0]
-        if len(args) >= 2:
-            self.width = args[1]
-        if len(args) >= 3:
-            self.height = args[2]
-        if len(args) >= 4:
-            self.x = args[3]
-        if len(args) >= 5:
-            self.y = args[4]
+        if args:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.width = args[1]
+            if len(args) >= 3:
+                self.height = args[2]
+            if len(args) >= 4:
+                self.x = args[3]
+            if len(args) >= 5:
+                self.y = args[4]
+        elif kwargs:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Inherits from rectangle class also defining Square class"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """"""
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
+        self.size = size
+    
+    @property
+    def size(self):
+        """size getter"""
+        return self.width
+    
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
+    
+    def __str__(self):
+        """__str__ method that returns [Square] (<id>)
+        <x>/<y> - <size>"""
+        return "[Square] ({}) {}/{} - {}".format(self.id,
+                                                 self.x, self.y,
+                                                 self.size)
