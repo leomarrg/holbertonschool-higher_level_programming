c9da27d595ce70d5f8b06eb340dfa42d019964c0
leomar <leomar.rodriguez1@upr.edu>
2024-02-16 14:07:04 -0400

    fixed pycodestyle errors

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -7,17 +7,17 @@ class Square(Rectangle):
     """Represents a square"""
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
-    
+
     @property
     def size(self):
         """size getter"""
         return self.width
-    
+
     @size.setter
     def size(self, value):
         self.width = value
         self.height = value
-    
+
     def __str__(self):
         """__str__ method that returns [Square] (<id>)
         <x>/<y> - <size>"""
