a3a8aba1d1fa620c77ba5e304b6fdd59aaca5be1
leomar <leomar.rodriguez1@upr.edu>
2024-02-16 14:06:17 -0400

    added documentation

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -4,10 +4,9 @@ from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """"""
+    """Represents a square"""
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
-        self.size = size
     
     @property
     def size(self):
