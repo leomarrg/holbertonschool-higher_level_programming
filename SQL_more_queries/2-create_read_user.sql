d004e922c2f759e9ae8943aac440be08eece11e1
leomar <leomar.rodriguez1@upr.edu>
2024-03-15 16:29:19 -0400

    deleted what was added added flush priv

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,5 @@
 -- creates database and user with select priv --
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT on 'hbtn_0d_1'.* TO  'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON 'hbtn_0d_2'.* TO  'user_0d_2'@'localhost';
+FLUSH PRIVILEGES;
