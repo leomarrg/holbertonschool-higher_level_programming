376bc37232fab09f1e0f47ef85bd2de714033e40
leomar <leomar.rodriguez1@upr.edu>
2024-03-15 14:29:51 -0400

    fixed errors in synta

--- SQL_introduction/11-best_score.sql
+++ SQL_introduction/11-best_score.sql
@@ -1,5 +1,2 @@
 -- script that lists all records with score above 10 --
-SELECT `name`, `score`
-FROM second_table
-WHERE `score` >= 10
-ORDER BY `score` DESC;
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- lists the number of records with the same score --
-SELECT COUNT(score) as number FROM second_table WHERE score=score;
+SELECT score, COUNT(score) as number FROM second_table GROUP BY score ORDER BY score DESC;
