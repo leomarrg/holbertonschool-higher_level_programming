0b82117448bd420512e493e9098e7285d3f9b7a8
leomar <leomar.rodriguez1@upr.edu>
2024-03-15 14:59:17 -0400

    added that the users has a password assign and if the user exists dont create ir so script doesnt fail

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,2 +1,4 @@
 -- creates user for MySQL server --
-CREATE USER 'user_0d_1'@'localhost';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+
