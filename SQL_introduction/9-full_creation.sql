f9b367833a26fa958a878c8482fd0091838e5ac8
leomar <leomar.rodriguez1@upr.edu>
2024-03-15 13:13:20 -0400

    added coment in the first line

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,5 +1,5 @@
+-- script that creates a table and adds multiple rows --
 CREATE TABLE IF NOT EXISTS second_table (id int, name VARCHAR(256), score int);
-
 INSERT INTO second_table (id, name) VALUES (1, 'John', 10);
 INSERT INTO second_table (id, name) VALUES (2, 'Alex', 3);
 INSERT INTO second_table (id, name) VALUES (3, 'Bob', 14);
