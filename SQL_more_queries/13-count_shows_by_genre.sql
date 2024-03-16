9b6ba049cadbda30dac5206421abbe51aa5671a3
leomar <leomar.rodriguez1@upr.edu>
2024-03-15 23:48:45 -0400

    fixes on task 13 and 14

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,6 @@
 -- lists all genres and displayes number of shows linked to each --
-SELECT tv_show_genres.genre_id AS genre,
-       COUNT(tv_show_genres.show_id) AS number_of_shows
-FROM tv_show_genres
+SELECT name AS genre, COUNT(*) AS number_of_shows FROM tv_genres
+JOIN tv_show_genres ON id=tv_show_genres.genre_id
 GROUP BY tv_show_genres.genre_id
 ORDER BY number_of_shows DESC;
 
--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,8 +1,7 @@
 --uses hbtn_od_tvshows to lists all genres of Dexter --
-SELECT tv_genres.name
-FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+-- lists all genres of the show Dexter --
+SELECT name FROM tv_genres
+JOIN tv_show_genres ON id=tv_show_genres.genre_id
+JOIN tv_shows ON tv_shows.id=tv_show_genres.show_id
 WHERE tv_shows.title = 'Dexter'
-ORDER BY tv_genres.name ASC;
-
+ORDER BY name;
