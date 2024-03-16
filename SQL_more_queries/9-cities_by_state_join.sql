11bda3574d75bf3fd6249dea59dd8778320334c0
leomar <leomar.rodriguez1@upr.edu>
2024-03-15 23:28:54 -0400

    changed cities to c and added join

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,6 @@
 -- lists all cities contained in database hbtn_0d_usa --
-SELECT cities.id, cities.name, states.name, FROM hbtn_0d_usa ORDER BY cities.id;
+SELECT c.`id`, c.`name`, s.`name`
+FROM `cities` AS c
+INNER JOIN `states` AS s
+ON c.`state_id` = s.`id`
+ORDER BY c.`id`;
