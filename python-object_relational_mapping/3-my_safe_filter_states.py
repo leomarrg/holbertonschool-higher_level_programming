66d89a063e58f43eceb6f74f116036572affb540
leomar <leomar.rodriguez1@upr.edu>
2024-03-21 03:51:56 -0400

    added -- to protect from injections

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Lists states that user inputs rom database hbtn_0e_0_usa
+"""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    name = sys.argv[3]
+    user_input = sys.argv[4]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        user=username,
+        passwd=password,
+        db=name,
+        port=3306
+    )
+
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM states WHERE \
+                name = '{}' ORDER BY id--".format(user_input))
+
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
