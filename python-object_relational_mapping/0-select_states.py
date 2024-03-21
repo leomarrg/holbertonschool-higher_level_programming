6e60f484d87999bf58e8a0909a28a6acea46cc1a
leomar <leomar.rodriguez1@upr.edu>
2024-03-20 23:37:04 -0400

    fixed pycodestyle errors

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -2,20 +2,21 @@
 import sys
 import MySQLdb
 
-
 if __name__ == "__main__":
-
     username = sys.argv[1]
     password = sys.argv[2]
     name = sys.argv[3]
 
-    db = MySQLdb.connect(host="localhost", user=username,
-                        passwd=password, db=name, port = 3306)
-    #cursos object
-    #execute all queries you need
+    db = MySQLdb.connect(
+        host="localhost",
+        user=username,
+        passwd=password,
+        db=name,
+        port=3306
+    )
+
     cur = db.cursor()
 
-    #Use all SQL you like
     cur.execute("SELECT * FROM states ORDER BY id ASC")
 
     rows = cur.fetchall()
@@ -24,4 +25,3 @@ if __name__ == "__main__":
 
     cur.close()
     db.close()
-
