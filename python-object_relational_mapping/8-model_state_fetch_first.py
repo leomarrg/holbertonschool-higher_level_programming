6f6d2b83e96c6a51eea1704e28446f49e3ce5f4f
leomar <leomar.rodriguez1@upr.edu>
2024-03-22 13:16:07 -0400

    list first state

---
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" lists all State objects """
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    """ Access to the database and get the states
+    from the database. """
+
+    dtb = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        argv[1], argv[2], argv[3])
+    engine = create_engine(dtb)
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+
+    state = session.query(State).order_by(State.id)
+
+    if state:
+        print('{}: {}'.format(state.id, state.name))
