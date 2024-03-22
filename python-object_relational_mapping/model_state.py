1f3379ac9fef0da3fdcb73d9e4b6fa54114d7ff5
leomar <leomar.rodriguez1@upr.edu>
2024-03-22 11:31:16 -0400

    deleted attributes in id

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
-"""
-Class defintino of State
-"""
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String
+""" define State class """
 
-Base = declarative_base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import declarative_base
+
+
+Base = declarative_base()
 
 
 class State(Base):
-    """
-    State class
-    """
+    """ State class """
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
