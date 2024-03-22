dd952be519be56d29a054035f89d6bab4142e69a
leomar <leomar.rodriguez1@upr.edu>
2024-03-22 15:22:36 -0400

    updated state with id 2 to new mexico

---
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" lists all State objects """
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    """ Access to the database and get the states
+    from the database. """
+
+    username = argv[1]
+    password = argv[2]
+    database = argv[3]
+
+    dtb = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        username, password, database)
+
+    engine = create_engine(dtb)
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+
+    new_state = session.get(State, 2)
+    new_state.name = "New Mexico"
+    session.commit()
